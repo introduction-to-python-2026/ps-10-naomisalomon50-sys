fc56c96f709309509324924fe14480038640d4f6
naomisalomon50-sys <naomi.salomon50@gmail.com>
2026-01-11 19:28:01 +0200

    Remove image display from main.py
    Removed image display line from main.py.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from image_utils import load_image, edge_detection
-
 sunset = load_image("/content/111713392_2901319196658224_5294982908021128204_n.png")
 clean_image = median(sunset, ball(3))
 edge_detection_clean_image = edge_detection(clean_image)
 
 threshold = 50
 binary = (edge_detection_clean_image > threshold).astype(int)
-plt.imshow(binary, cmap='gray')
